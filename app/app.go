c202e864a3ab1e2aeeda3d861ac9417bc3e80d54
TimNhanTa <tim@unigrid.org>
2023-03-07 18:11:54 +0100

    Change to import chain github url

--- app/app.go
+++ app/app.go
@@ -104,9 +104,10 @@ import (
 	dbm "github.com/tendermint/tm-db"
 
 	// this line is used by starport scaffolding # stargate/app/moduleImport
+	ugdvestingante "github.com/timnhanta/ugdvesting/x/dex/ante"
 
-	appparams "ugdvesting/app/params"
-	"ugdvesting/docs"
+	appparams "github.com/timnhanta/ugdvesting/app/params"
+	"github.com/timnhanta/ugdvesting/docs"
 )
 
 const (
@@ -678,7 +679,7 @@ func New(
 	app.SetInitChainer(app.InitChainer)
 	app.SetBeginBlocker(app.BeginBlocker)
 
-	anteHandler, err := ante.NewAnteHandler(
+	anteHandler, err := ugdvestingante.NewAnteHandler(
 		ante.HandlerOptions{
 			AccountKeeper:   app.AccountKeeper,
 			BankKeeper:      app.BankKeeper,
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module ugdvesting
+module github.com/timnhanta/ugdvesting
 
 go 1.19
 
 require (
 	github.com/cosmos/cosmos-sdk v0.46.7
+	github.com/cosmos/go-bip39 v1.0.0
 	github.com/cosmos/ibc-go/v6 v6.1.0
 	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/mux v1.8.0
@@ -46,7 +47,6 @@ require (
 	github.com/confio/ics23/go v0.9.0 // indirect
 	github.com/cosmos/btcutil v1.0.5 // indirect
 	github.com/cosmos/cosmos-proto v1.0.0-alpha8 // indirect
-	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/cosmos/iavl v0.19.4 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.12.1 // indirect
