1851fdd57d9077039cad8836d7be2987b3842ffd
TimNhanTa <tim@unigrid.org>
2023-03-22 16:43:41 +0100

    add test for connection to hedgehog

--- x/hedgehogvesting/types/rest.go
+++ x/hedgehogvesting/types/rest.go
@@ -15,27 +15,35 @@ func HegdehogRequestGetVestingByAddr(addr string) *Vesting {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
+
 	client := &http.Client{Transport: tr}
 
 	resp, err := client.Get(HedgehogBaseUrlTest + addr)
-	if resp.StatusCode == http.StatusNotFound {
-		return nil
-	}
 	if err != nil {
 		panic(err)
 	}
 
 	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		panic(err)
+	}
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	var response *Vesting
-	err = json.Unmarshal([]byte(body), &response)
+	if len(body) == 0 {
+		return nil
+	}
+
+	var vesting *Vesting
+
+	err = json.Unmarshal([]byte(body), &vesting)
 	if err != nil {
 		panic(err)
 	}
 
-	return response
+	return vesting
 }
